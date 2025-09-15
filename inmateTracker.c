31386a7d9278303e82affb82076f9cd9d987fb21
jlu0519 <lukeyoung0519@gmail.com>
2025-09-14 21:49:02 -0700

    Implemented struct corrected and created inmate.

--- inmateTracker.c
+++ inmateTracker.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
 enum Classification{GP, PC, ADSEG, CIVIL, SVP};
 
-int main(int argc,char *argv[]){
-   
-
-    struct inmate{
-        int bookingNumber;
+struct Inmate{
         int age;
         enum Classification Class;
-        char firstName[];
-        char lastName[];
+        char firstName[25];
+        char lastName[25];
 
     };
 
+int main(int argc,char *argv[]){
+   
+        struct Inmate john;
+        john.age =25;
+        john.Class = GP;
+        strcpy(john.firstName,  "John");
+        strcpy(john.lastName, "Miller");
+
+
+        printf("John is %d years old!\n", john.age);
+
+    
+    
 
     return 0;
 }
