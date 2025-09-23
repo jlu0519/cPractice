f5020dcd3820380d70a59a3ea63aa2c74ab3154e
jlu0519 <lukeyoung0519@gmail.com>
2025-09-22 22:28:29 -0700

    Basic framework for stringFunctions.c added

--- week3/stringFunctions.c
+++ week3/stringFunctions.c
@@ -1,14 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 
+// Functions Intialized
+void strCat(string[50]);
 
-void functionCat(string[50]);
+int slength(name[50]); 
 
+bool strCompare(name[50]);
+
+// Main program
 int main(){
+    const char name[] = "Luke Young";
+    const char middleName = "Timothy";
+
 
 }
 
-
-void functionCat(string[50]){
+// Function defintions
+void strCat(string[50]) {
 
 }
+
+int sLength(name[50]) {
+
+}
+
+bool strCompare(name[50]) {
+
+}
+
